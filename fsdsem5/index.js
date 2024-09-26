257e7c3625368c17104868b1213655f444fe468d
aa311003 <anantagarwal2003@gmail.com>
2024-09-26 22:56:41 +0530

    user authentication executed

--- fsdsem5/index.js
+++ fsdsem5/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 
 const taskRouter = require("./routes/taskRouter");
+const userRouter = require("./routes/userRouter");
 
 const {MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require("./config/config");
 const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
@@ -21,13 +22,15 @@ mongoose.connect(
     app.use(express.json());
 
     app.get("/",(req,res)=>{
-        res.send("<h1>Hello world using Docker and Express</h1>");
-    });
+        res.send("<h1>Hello world using Docker and Express!!!</h1>");
+});
 
-    app.use("/api/v1/tasks",taskRouter);
+app.use("/api/v1/tasks",taskRouter);
 
-    const PORT = process.env.PORT || 3000;
+app.use("/api/v1/users",userRouter);
 
-    app.listen(PORT, ()=>{
-        console.log(`Server Started on POR: ${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server Started on POR: ${PORT}`);
 })
